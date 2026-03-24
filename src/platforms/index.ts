f7ae9bc844fcefcf0a8144c3c55ceea776c5780a
Sniezka1927 <dygons14@gmail.com>
2026-03-24 15:29:33 +0100

    Add Titan Solana limit orders integration

--- src/platforms/index.ts
+++ src/platforms/index.ts
@@ -17,6 +17,7 @@ import orePlatform from './ore'
 import ratexPlatform from './ratex'
 import raydiumPlatform from './raydium'
 import realmsPlatform from './realms'
+import titanPlatform from './titan'
 import yuzuPlatform from './yuzu'
 
 export const platforms = [
@@ -36,6 +37,7 @@ export const platforms = [
   ratexPlatform,
   raydiumPlatform,
   realmsPlatform,
+  titanPlatform,
   yuzuPlatform,
   canopyPlatform,
   echelonPlatform,
